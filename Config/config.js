dff12aa9984f15b66f0d5cc6a26d27884f416eac
swaroop <swaroopaee98@gmail.com>
2022-08-31 20:09:37 +0530

    register route added

--- Config/config.js
+++ Config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  mongo:process.env.MONGODB_CONNECTION
+  mongo:process.env.MONGODB_CONNECTION,
+  secretkey:process.env.SECRET_KEY
 };
 
 
--- models/user.js
+++ models/user.js
@@ -1,12 +1,22 @@
-import mongoose from 'mongoose';
+const mongoose=require('mongoose');
 const { Schema,model } = mongoose;
 
 
 const userSchema=new Schema({
     name:{
-        type: 'string',
+        type:'string',
+        required: true,
+    },
+    email:{
+        type:'string',
+        required: true,
+        
+    },
+    password:{
+        type:'string',
         required: true,
     }
+    
 })
 
 module.exports=model('user',userSchema);
