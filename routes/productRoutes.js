9a298a36405e4722346b2c850015c47b5426f97d
swaroop <swaroopaee98@gmail.com>
2023-03-12 16:53:14 +0530

    Get All Products  function bug fixed

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,7 +11,7 @@ route.route('/product/:id').post(isLoggedIn,customRoleChecker("Admin"),updatePro
                            .delete(isLoggedIn,customRoleChecker("Admin"),deleteProductbyId)
 
 //doesnt require admin previliges
-route.route('/products').post(getAllProduct);
+route.route('/products').get(getAllProduct);
 
 
 
