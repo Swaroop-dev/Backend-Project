e7a983065485c4bf3008c1ca09b1a935b579ded3
swaroop <swaroopaee98@gmail.com>
2023-03-19 21:17:44 +0530

    Payment Route

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,7 +1,32 @@
-const { stripePublishKey } = require("../Config/config");
+const { stripePublishKey, stripeSecretKey } = require("../Config/config");
 const BigPromise = require("../middlewares/BigPromise");
+
+const stripe = require('stripe')(stripeSecretKey)
 
 exports.getPaymentPublishKey=BigPromise((req,res,next)=>{
     return res.status(200).json({key:stripePublishKey
     })
 })
+
+
+exports.paymentInit=BigPromise(async(req,res,next)=>{
+    const {price}=req.body
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: 'inr',
+              product_data: {
+                name: 'T-shirt',
+              },
+              unit_amount: price,
+            },
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+      });
+    
+      res.status(200).json({message:" payment successful"});
+    }
+)
